e4c96e8cb5d89a74128bed38058ce6685b5232b2
Christopher Pietsch <cpietsch@gmail.com>
2016-02-03 16:02:35 +0100

    php write to file fails

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// Warning: fopen(./logs/dkk0j09a153uskclafqs707ki6.csv): failed to open stream: Permission denied in /var/customers/webs/ucl/fw4beta/index.php on line 9 Warning: fwrite() expects parameter 1 to be resource, boolean given in /var/customers/webs/ucl/fw4beta/index.php on line 10
 session_start();
 
 // create empty log file or add nothing
