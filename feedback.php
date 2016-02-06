eaff0aac3e80a00e3f347a1c003498f330247f4d
Christopher Pietsch <cpietsch@gmail.com>
2016-02-06 17:55:51 +0100

    feedback masking

---
+++ feedback.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>FW4 Feedback</title>
+<style type="text/css">
+	body, html {
+		border: 0;
+		margin: 0;
+		overflow: hidden;
+		widows: 100%;
+		height: 100%;
+	}
+	iframe {
+		border: none;
+		width: 100%;
+		height: 100%;
+	}
+</style>
+</head>
+
+<body>
+	<iframe src="https://fw4.typeform.com/to/ZIIgIe?cid=<?php echo session_id(); ?>">go to <a href="https://fw4.typeform.com/to/ZIIgIe?cid=<?php echo session_id(); ?>">https://fw4.typeform.com/to/ZIIgIe?cid=<?php echo session_id(); ?></a></iframe>
+</body>
