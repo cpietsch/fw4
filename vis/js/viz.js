5b0f525733ec8475f1908365497ee7d755e1c9dd
Christopher Pietsch <cpietsch@gmail.com>
2016-02-09 21:52:09 +0100

    local

--- vis/js/viz.js
+++ vis/js/viz.js
@@ -1,4 +1,3 @@
-
 //                                   ,--,                                     ,----, 
 //        ,---,.           .---.      ,--.'|                       ,---,       .'   .`| 
 //      ,'  .' |          /. ./|   ,--,  | :               ,---.,`--.' |    .'   .'   ; 
@@ -13,7 +12,7 @@
 //    |   :  \      :   '  |--"       |  : ;             :   \ |;   |.' |   :     .'    
 //    |   | ,'       \   \ ;          '  ,/               '---" '---'   ;   |  .'       
 //    `----'          '---"           '--'                              `---'           
-                                                                                  
+
 // christopher pietsch
 // cpietsch@gmail.com
 // tweet me @chrispiecom
@@ -24,90 +23,141 @@
 
 // utils.welcome();
 
-var local = true;
-var s3 = local ? (lang=="en" ? "../../vis/" : "") : "http://s3.eu-central-1.amazonaws.com/fw4/";
+var local = false;
+var s3 = local ? (lang == "en" ? "../../vis/" : "") : "http://s3.eu-central-1.amazonaws.com/fw4/";
 
 var data;
 var imagesMap;
 var imagesMap2 = d3.map([]);
-
+var cloud;
+var list;
 var c = console.log.bind(console);
-
-var cloud = myTagCloud();
-var list = myListView();
-
-var ping = utils.ping();
+var ping;
 var logger = Logger().register("vis");
 
 logger.log({ action: "enter vis" });
 
-d3.csv(s3 + "data/timeline.csv", function(timeline){
-d3.csv(s3 + "data/themen.csv", function(texte){
-d3.csv(s3 + "data/transKeyword.csv", function(transKeyword){
-Loader(s3 + "data/spsg.csv").finished(function(data){
-Loader(s3 + "data/neu_100.csv").finished(function(images){
-  logger.log({ action: "loaded csv" });
+if(utils.isMobile()){
+  logger.log({ action: "mobile" }).sync();
+  alert("come back in some weeks");
+} else {
+  if (Modernizr.webgl) {
+      init();
+    } else {
+      logger.log({ action: "noWebGL" }).sync();
+      alert("sorry you need the latest browser") 
+    }
+}
 
-  window.data = data;
+function init() {
 
-  // c(texte)
-  // data cleaning
-  utils.clean(data,texte,transKeyword);
+    cloud = myTagCloud();
+    list = myListView();
+    ping = utils.ping();
 
-  // utils.printkeyowords(data);
+    logger.log({ action: "load" });
 
-  // imagesMap = d3.map(utils.fakeDataSmall(data), function(d){ return d.id; });
-  // imagesMap2 = d3.map(utils.fakeDataBig(data), function(d){ return d.id; });
+    d3.csv(s3 + "data/timeline.csv", function(timeline) {
+        d3.csv(s3 + "data/themen.csv", function(texte) {
+            d3.csv(s3 + "data/transKeyword.csv", function(transKeyword) {
+                Loader(s3 + "data/spsg.csv").finished(function(data) {
+                    Loader(s3 + "data/neu_100.csv").finished(function(images) {
+                        logger.log({
+                            action: "loaded"
+                        });
 
-  imagesMap = d3.map(images, function(d){ return d.id; });
-  Loader(s3 + "data/neu_1000.csv").finished(function(images2){
-    imagesMap2 = d3.map(images2, function(d){ return d.id; });
-  });
+                        window.data = data;
+
+                        // c(texte)
+                        // data cleaning
+                        utils.clean(data, texte, transKeyword);
+
+                        // utils.printkeyowords(data);
+
+                        if (!local) {
+                            imagesMap = d3.map(images, function(d) {
+                                return d.id;
+                            });
+                            Loader(s3 + "data/neu_1000.csv").finished(function(images2) {
+                                imagesMap2 = d3.map(images2, function(d) {
+                                    return d.id;
+                                });
+                            });
+                        } else {
+                            imagesMap = d3.map(utils.fakeDataSmall(data), function(d) {
+                                return d.id;
+                            });
+                            imagesMap2 = d3.map(utils.fakeDataBig(data), function(d) {
+                                return d.id;
+                            });
+                        }
+
+                        cloud.init(data);
+
+                        // list.loadTimeline(timeline);
+                        list.init(data, timeline);
 
 
-  cloud.init(data);
-  
-  // list.loadTimeline(timeline);
-  list.init(data,timeline);
+                        cloud.mouseenterCallback(function(d) {
+                            list.highlight(d);
+                        })
+
+                        cloud.mouseclickCallback(function(d) {
+                            list.flip(d);
+                        })
 
 
-  cloud.mouseenterCallback(function(d){
-    list.highlight(d);
+
+                    });
+                });
+            });
+        });
+    });
+
+    d3.select(window)
+        .on("resize", function() {
+            clearTimeout(window.resizedFinished);
+            window.resizedFinished = setTimeout(function() {
+                list.resize();
+                cloud.resize();
+                logger.log({
+                    action: "resize",
+                    target: window.innerWidth + "," + window.innerHeight
+                });
+            }, 250);
+        })
+
+    
+
+}
+
+d3.select("body").attr("class", lang);
+
+d3.select(".slidebutton")
+  .on("click", function(){
+    var s = !d3.select(".sidebar").classed("sneak");
+    d3.select(".sidebar").classed("sneak", s);
+    logger.log({ action: !s ? "open" : "close" , target: "detail" });
   })
 
-  cloud.mouseclickCallback(function(d){
-    list.flip(d);
+d3.select(".infobutton")
+  .on("click", function(){
+    var s = !d3.select(".infobar").classed("sneak");
+    d3.select(".infobar").classed("sneak", s)
+    logger.log({ action: !s ? "open" : "close" , target: "info" });
   })
-
-
- 
-});
-});
-});
-});
-});
-
-d3.select(window)
-  .on("resize", function(){
-    clearTimeout(window.resizedFinished);
-      window.resizedFinished = setTimeout(function(){
-          list.resize();
-          cloud.resize();
-          logger.log({ action: "resize", target: window.innerWidth + "," + window.innerHeight });
-      }, 250);
-  })
-
- d3.select("body")
-    .attr("class", lang);
+Ps.initialize(d3.select('.infobar .outer').node());
 
 window.onbeforeunload = function() {
     // todo: tracking stuff
-    logger.log({ action: "close" });
+    logger.log({
+        action: "close"
+    });
     logger.sync();
 };
 
 
-Ps.initialize(d3.select('.infobar .outer').node());
+
 
 
 
@@ -116,5 +166,4 @@ Ps.initialize(d3.select('.infobar .outer').node());
 //   console.error(message, lineNumber, url);
 //   logger.log({ action: "error", target: lineNumber + ": " + message });
 //   return true;
-// };  
-
+// };
